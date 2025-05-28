5729e48fd0f1fe11fb2a4fb45a0cac49f9ea3ed6
Mohsin Khan <mariamkhan33000@gmail.com>
2025-05-28 11:20:52 +0500

    Create Button Token and UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [token, setToken] = useState(true)
   return (
     <div className=''>
         <div className='flex justify-between items-center bg-white text-sm py-4 mb-5 border-b border-b-gray-400'>
@@ -27,10 +32,26 @@ const Navbar = () => {
 
             </ul>
 
-            <div>
-                <button>
+            <div className='flex items-center gap-4'>
+
+                {
+                    token ? 
+                    <div className='flex items-center gap-2 cursor-pointer group relative'>
+                        <img src= {assets.profile_pic} alt="" className='w-8 rounded-full' />
+                        <img src= {assets.dropdown_icon} alt="" className='w-2.5 ' />
+
+                        <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                            <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                            <p onClick={(() => navigate('/my-profile'))} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={(() => navigate('/my-appointments'))} className='hover:text-black cursor-pointer'>My Appointments</p>
+                            <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                            </div>
+                        </div>
+                    </div> :  <button onClick={() => navigate("/login")} className='text-white bg-primary px-8 py-3 rounded-full font-light hidden md:block'>
                     Create Accout
                 </button>
+                }
+                
             </div>
         </div>
     </div>
