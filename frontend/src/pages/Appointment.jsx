9e74b83a1b5a2b75569776d7c0273bd0a0bf76ce
Mohsin Khan <mariamkhan33000@gmail.com>
2025-05-31 16:42:50 +0500

    Date time Hour arrow function

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -9,6 +9,9 @@ const Appointment = () => {
   const {docId} = useParams()
 
   const [docInfo, setDocInfo] = useState(null)
+  const [docSlots, setDocSlots] = useState([])
+  const [slotIndex, setSlotIndex] = useState(0)
+  const [slotTime, setSlotTime] = useState('')
 
   const fetchDocInfo = async () => {
     const docInfo = doctors.find(doc => doc._id === docId)
@@ -16,10 +19,64 @@ const Appointment = () => {
     console.log(docInfo)
   }
 
+  const getAvailableSlots = async () => {
+    setDocSlots([])
+
+    // getting current date
+    let today = new Date()
+
+    for(let i = 0; i < 7; i++) {
+      //getting date with index
+      let currentDate = new Date(today)
+      currentDate.setDate(today.getDate()+i)
+
+      // setting end time of the date with index
+      let endTime = new Date()
+      endTime.setDate(today.getDate()+i)
+      endTime.setHours(21,0,0,0)
+
+      // setting hours
+      if(today.getDate() === currentDate.getDate()){
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
+      } else {
+        currentDate.setHours(10)
+        currentDate.setMinutes(0)
+      }
+
+      let timeSlots = []
+
+      while(currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+
+        //  add slot to array
+        timeSlots.push({
+          dateTime : new Date(currentDate),
+          time: formattedTime
+        })
+
+
+        //Increment current time by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() +30)
+      }
+
+      setDocSlots(prev => ([...prev, timeSlots]))
+    }
+  }
+
   useEffect(() => {
     fetchDocInfo()
   }, [doctors, docId])
+
+
+  useEffect(() => {
+    getAvailableSlots()
+  },[docInfo])
   
+
+  useEffect(() => {
+    console.log(docSlots)
+  },[docSlots])
   return docInfo && (
     <div>
       {/*  ----------Doctor Details----------- */}
